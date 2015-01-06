9d9890912ad7cf4bcbc5b1729c83d3014c3c256c
David Boon <daveb@porch.com>
2015-01-06 05:35:20 -0800

    change colors of mode-buffer-id depending on evil mode.

--- emacs/emacs.d/setup-evil-mode.el
+++ emacs/emacs.d/setup-evil-mode.el
@@ -21,5 +21,22 @@
 ;; (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 ;; (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
+
 (define-key evil-normal-state-map (kbd "C-r") 'isearch-backward) 
+
+
+   ;; change mode-line color by evil state
+   (lexical-let ((default-color (cons (face-background 'mode-line)
+                                      (face-foreground 'mode-line))))
+     (add-hook 'post-command-hook
+       (lambda ()
+         (let ((color (cond ((minibufferp) default-color)
+                            ((evil-insert-state-p) '("#e80000" . "#ffffff"))
+                            ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
+                            ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+                            (t default-color))))
+           (set-face-background 'mode-line-buffer-id (car color))
+           (set-face-foreground 'mode-line-buffer-id (cdr color))))))
+
+
 (provide 'setup-evil-mode)
